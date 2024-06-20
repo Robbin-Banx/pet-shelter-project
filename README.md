7e275045a979dc45487a306c70a671f933c5adf6
Robbin-Banx <149735725+Robbin-Banx@users.noreply.github.com>
2024-06-21 00:58:04 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,86 @@
+
+# Pet Shelter Registry
+#### Video Demo: https://youtu.be/MDGXtWA3-iU
+
+*Author: Aleksandar Kostadinov
+
+*Github: Robbin-Banx
+
+*edx: a-kostadinov
+
+*Location: Sofia, Bulgaria
+
+*Date: 10.06.2024
+
+## Table of Contents
+
+- [Description](#description)
+- [Usage](#usage)
+- [Operation](#operation)
+- [Components](#components)
+
+#### Description:
+The project's main goal is to help create a registry for a pet shelter.
+To achieve this the program creates a CSV file named "database.csv" if one does not exist in the directory already. The name of the database can be changed on line 7 before starting the program.
+
+##### Interactive Mode
+Upon starting the program the user is prompted for input. The user can:
+* Type "read" to read the database, which will output a table of the contents of the csv file in a table formatting.
+* Type "search" to search the database and if a match is found the user can edit or remove the database entry by typing "edit" or "remove". This will not change the order of the other entries.
+* Type "write" to add a new patient to the database.
+* The user can also exit the program by typing "Exit".
+* These inputs are not case sensitive.
+
+##### Command-line mode:
+* "patient.py -r" to read the database, which will output a table of the contents of the csv file in a table formatting.
+* "patient.py -s" to search the database and if a match is found the user can edit or remove the database entry by typing "edit" or "remove". This will not change the order of the other entries.
+* "patient.py -w" to add a new patient to the database.
+
+#### Operation
+The program operates by creating a patient object using the Patient class which contains the species, gender, name and age of the pet. Then depending on the context the object can write to the database or edit the class instance by user prompts.
+
+#### Patient Class
+
+The `Patient` class is used to represent the pets in the shelter. Each instance of the `Patient` class includes attributes for the species, gender, name, and age of the pet. The class provides methods for:
+
+- Initializing a patient with given or prompted values.
+- Generating a string description of the patient.
+- Iterating over the patient's attributes to return them as a dictionary.
+- Comparing two patient objects for equality based on their attributes.
+- Writing the patient's data to a CSV file.
+- Editing the patient's attributes interactively.
+
+ **Attributes:** `species`, `gender`, `name`, `age`
+ **Methods:**
+    - `__str__`: Returns a description of the pet.
+    - `__iter__`: Returns a dictionary where the keys are `"species"`, `"gender"`, `"name"`, and `"age"`, and the values are the corresponding strings or integers.
+    - `__eq__`: Supports comparison of different instances.
+
+#### Creating Objects
+
+- **Interactive Mode:** Call the `Patient()` function. The user is prompted to input the necessary data for creating the object.
+- **Search Mode:** When a search of the CSV is conducted and a result is found, the search function creates an object by calling `Patient("species", "gender", "name", "age")` and returns the created object.
+
+#### Main Function
+
+The `main` function manages the primary workflow of the program. It ensures that the CSV file exists and handles user inputs through both command-line arguments and interactive prompts. The function supports reading the database, searching for and modifying entries, and adding new patients to the database.
+
+#### Write Function
+
+The `write` function creates a new `Patient` instance and prompts the user to review the data. If confirmed, the patient data is written to the database. It provides an option to exit the program or retry the process.
+
+#### Search Base Function
+
+The `search_base` function searches the database for a patient by name. It returns the matching patient object if found, allowing further actions like editing or removal. The function handles cases with no results, single results, or multiple results.
+
+#### Multiple Search Results Function
+
+The `multiple_search_results` function is only called by the `search_base` function in scenarios where multiple patients match the search criteria. It prompts the user to select the correct patient from the list of found items and returns the chosen patient object for further actions.
+
+#### Edit Entry Function
+
+The `edit_entry` function allows editing of an existing patient's details. It renames the current database file and creates a new one while preserving all other entries. The function updates the database with the edited patient information or reverts to the original if editing fails.
+
+#### Remove Entry Function
+
+The `remove_entry` function removes a specified patient from the database. It renames the current database file and creates a new one, excluding the removed patient's data. The function ensures the remaining data and order preserved in the new database file.
---
+++ project.py
@@ -0,0 +1,427 @@
+"""
+Pet Shelter Registry
+=====================
+
+This file is a basic program to keep track of the patients of a pet shelter. This is a final project for the CS50P course, led by David A. Malan.
+
+Author: Aleksandar Kostadinov
+Github: Robbin-Banx
+edx: a-kostadinov
+Location: Sofia, Bulgaria
+Date: 10.06.2024
+
+"""
+
+import sys
+import csv
+import re
+import os
+from tabulate import tabulate
+
+file_name = "database.csv"
+
+
+class Patient:
+    """"
+    A class used to represent types of patients.
+
+
+    """
+
+    def __init__(
+        self, species: str = None, gender: str = None, name: str = None, age: int = None
+    ):
+        if species != None:
+            self.species = species
+        else:
+            self.species = input("What is the patient's species? ").capitalize()
+
+        if gender != None:
+            self.gender = gender
+        else:
+            self.gender = input("What is the patient male or female? ").lower()
+
+        if name != None:
+            self.name = name
+        else:
+            self.name = input("What is patient's name? ").capitalize()
+
+        if age != None:
+            self.age = age
+        else:
+            self.age = input("What's the patient's age? ")
+
+    def __str__(self):
+        return str(f"Patient is a {self.gender} {self.species}. Patient's name is {self.name} and is {self.age} years old.")
+
+    def __iter__(self):
+        yield "Species", self.species
+        yield "Gender", self.gender
+        yield "Name", self.name
+        yield "Age", self.age
+
+    def __eq__(self, other):
+        # don't attempt to compare against unrelated types
+        if not isinstance(other, Patient):
+            return NotImplemented
+
+        return (
+            self.species == other.species and
+            self.gender == other.gender and
+            self.name == other.name and
+            self.age == other.age
+        )
+
+    @property
+    def species(self):
+        return self._species
+
+    @species.setter
+    def species(self, value):
+        value = value.capitalize()
+        if value == "Dog" or value == "Cat":
+            self._species = value
+        else:
+            raise ValueError("Patient can be a dog or a cat.")
+
+    @property
+    def gender(self):
+        return self._gender
+
+    @gender.setter
+    def gender(self, value):
+        if value == "male" or value == "female":
+            self._gender = value
+        else:
+            raise ValueError("Patient must be male or female.")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if re.search(r"\w+", value):
+            self._name = value
+        else:
+            raise ValueError("Name must be at least one word")
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if re.search(r"\d+", value):
+            self._age = value
+        else:
+            raise ValueError("Age must be a number")
+
+    def write(self, silent: bool = False):
+
+        with open(file_name, "r") as file:
+            reader = csv.DictReader(file)
+            file_keys = reader.fieldnames
+
+        keys = list(dict(self).keys())
+
+        try:
+            with open(file_name, "a", newline="") as file:
+                writer = csv.DictWriter(file, fieldnames=keys)
+                if file_keys != keys:
+                    writer.writeheader()
+                writer.writerow(
+                    {
+                        "Species": self.species,
+                        "Gender": self.gender,
+                        "Name": self.name,
+                        "Age": self.age,
+                    }
+                )
+        finally:
+            if silent == True:
+                pass
+            else:
+                print("Write successful.")
+
+    def edit(self, silent: bool = False):
+
+        if silent == False:
+            print(self)
+
+        while True:
+            match input("Do you want to edit species, gender, name or age?: ").lower():
+                case "species":
+                    if self.species == "Dog":
+                        self.species = "Cat"
+                        print("Species changed to Cat.")
+                        break
+                    elif self.species == "Cat":
+                        self.species = "Dog"
+                        print("Species changed to Dog.")
+                        break
+                case "gender":
+                    if self.gender == "male":
+                        self.gender = "female"
+                        print("Gender changed to female.")
+                        break
+                    elif self.gender == "female":
+                        self.gender = "male"
+                        print("Gender changed to male.")
+                        break
+                case "name":
+                    self.name = input("What is the new name?: ")
+                    break
+                case "age":
+                    self.age = input("What is the new age?: ")
+                    break
+                case _:
+                    print("Field not found")
+
+        if silent == False:
+            print("New description is: ", self)
+
+        return self
+
+
+def main():
+    """Creates a file if one does not exist in the directory"""
+    try:
+        with open(file_name, "x") as file:
+            pass
+    except FileExistsError:
+        pass
+
+    """If command line arguments are preset execute them, if not prompts user for action, executes selected action"""
+    while True:
+
+        if len(sys.argv) == 2:
+            match sys.argv[1]:
+
+                case "-r":
+                    route = "read"
+
+                case "-s":
+                    route = "search"
+
+                case "-w":
+                    route = "write"
+
+                case _:
+                    sys.exit("Command-line input not recognized")
+
+        else:
+            route = input("Read, Search or Write to database?: ").lower()
+
+        match route:
+            case "read":
+                list = []
+
+                with open(file_name) as file:
+                    reader = csv.reader(file)
+                    for row in reader:
+                        list.append(row)
+                if len(list) > 0:
+                    print(
+                        tabulate(list, headers="firstrow", showindex=False, tablefmt="grid")
+                    )
+                    break
+                else:
+                    print("The database is empty")
+                    break
+
+            case "search":
+                found_patient = search_base(input("Name to search: ").capitalize())
+                if (found_patient != None):
+                    print(found_patient)
+
+                    type_of_change = input(
+                        "Do you want to edit or remove patient? Type 'edit', 'remove' or 'exit'! : ").lower()
+
+                    match type_of_change:
+
+                        case "edit":
+                            try:
+                                edit_entry(found_patient)
+                                break
+                            except ValueError as er:
+                                print(er)
+
+                        case "remove":
+                            remove_entry(found_patient)
+                            break
+                        case "exit":
+                            os.exit("Exiting.")
+                        case _:
+                            print("Input not recognized. Try again!")
+                else:
+                    print("No results found")
+                    break
+
+            case "write":
+                write()
+                break
+
+            case "exit":
+                sys.exit
+                break
+
+            case _:
+                print("Invalid choice. Type 'Read', 'Write' or 'Exit'?")
+                continue
+
+
+def write():
+
+    while True:
+        try:
+            patient = Patient()
+            break
+        except ValueError as er:
+            print(er)
+            if input("Do you want to try again? Type 'Y' or 'N'!").lower() == "y":
+                continue
+            else:
+                break
+
+    while True:
+        print(patient)
+        confirmation = input(
+            "Review data. Commit to database? Type 'Y' or 'N'! "
+        ).lower()
+
+        if confirmation == "y":
+            patient.write()
+            break
+        elif confirmation == "n":
+            patient.edit(silent = True)
+        elif input("Exit program?Type 'Y' or 'N'! ").lower() == "y":
+            sys.exit
+        else:
+            continue
+
+
+def search_base(search_condition):
+    found_items: list = []
+
+    try:
+        with open(file_name, "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row.get("Name")
+                if search_condition == name:
+                    species = row.get("Species")
+                    gender = row.get("Gender")
+                    age = row.get("Age")
+                    found_items.append(Patient(species, gender, name, age))
+                else:
+                    continue
+
+    except FileNotFoundError:
+        print("File not found:", file_name)
+    except Exception as ex:
+        print("An error occurred:", ex)
+
+    if len(found_items) == 0:
+        return None
+    elif len(found_items) == 1:
+        print("Search Successful")
+        return found_items[0]
+    elif len(found_items) > 1:
+        return multiple_search_results(found_items)
+
+
+def multiple_search_results(found_items: list):
+    # Handle cases where there are multiple found items from the search function
+    numbering: int = 1
+
+    # Create indexing for found results
+    for i in found_items:
+        print(numbering, i)
+        numbering += 1
+
+    while True:
+        # Ask user which match is the correct one
+        try:
+            user_input = int(
+                input("Which patient is the one you are looking for? Type the index number: "))
+            break
+        except ValueError:
+            print("Please input an integer")
+
+    user_input = user_input-1
+    return (found_items[user_input])
+
+def edit_entry(patient):
+
+    old_patient = Patient(patient.species, patient.gender, patient.name, patient.age)
+    new_patient = patient.edit()
+
+    os.rename(file_name, "database_old.csv")
+    with open(file_name, "x") as _:
+        pass
+
+    try:
+        with open("database_old.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                species = row.get("Species")
+                gender = row.get("Gender")
+                name = row.get("Name")
+                age = row.get("Age")
+                test_patient = Patient(species, gender, name, age)
+
+                if test_patient == old_patient:
+
+                    try:
+                        new_patient.write(silent=True)
+                        print("Patient editted")
+                    except TypeError:
+                        test_patient.write(silent=True)
+                        print("An error occured. No changes were made.")
+
+                else:
+                    test_patient.write(silent=True)
+
+        os.remove("database_old.csv")
+
+    except FileNotFoundError:
+        print("File not found:", file_name)
+    except Exception as ex:
+        print("An error occurred:", ex)
+        os.rename("database_old.csv", file_name)
+
+def remove_entry(patient):
+    # Rename database to database_old and create a new database file
+    os.rename(file_name, "database_old.csv")
+    with open(file_name, "x") as _:
+        pass
+
+    try:
+        with open("database_old.csv", "r") as file:
+            reader = csv.DictReader(file)
+            # Copy each patient except the one that is passed in
+            for row in reader:
+                species = row.get("Species")
+                gender = row.get("Gender")
+                name = row.get("Name")
+                age = row.get("Age")
+                test_patient = Patient(species, gender, name, age)
+
+                if test_patient == patient:
+                    print("Patient removed")
+                    pass
+
+                else:
+                    test_patient.write(silent=True)
+
+    except FileNotFoundError:
+        print("File not found:", file_name)
+    except Exception as ex:
+        print("An error occurred:", ex)
+    # Remove the old database
+    os.remove("database_old.csv")
+
+
+if __name__ == "__main__":
+    main()
